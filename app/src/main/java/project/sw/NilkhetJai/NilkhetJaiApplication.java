eb823e0cb9d1dbbe6dcb3216ae3a2ac979cda9f8
Motiur Munna <motiur.munna@dynamicmalloc.xyz>
2022-01-03 16:25:58 +0600

    initial project setup

---
+++ app/src/main/java/project/sw/NilkhetJai/NilkhetJaiApplication.java
@@ -0,0 +1,13 @@
+package project.sw.NilkhetJai;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NilkhetJaiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NilkhetJaiApplication.class, args);
+	}
+
+}
---
+++ app/src/test/java/project/sw/NilkhetJai/NilkhetJaiApplicationTests.java
@@ -0,0 +1,13 @@
+package project.sw.NilkhetJai;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NilkhetJaiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
