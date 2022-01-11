aa9872571c435527228d0f19b60ae87de70d13cb
Motiur Munna <motiur.munna@dynamicmalloc.xyz>
2022-01-12 02:41:00 +0600

    Login & registration test code

---
+++ app/src/test/java/project/sw/NilkhetJai/RegistrationTest.java
@@ -0,0 +1,49 @@
+package project.sw.NilkhetJai;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.boot.test.web.client.TestRestTemplate.HttpClientOption;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.util.LinkedMultiValueMap;
+import org.springframework.util.MultiValueMap;
+import static org.assertj.core.api.Assertions.assertThat;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class RegistrationTest {
+
+    public String regAppURL = "http://localhost:8027/registration";
+    public TestRestTemplate restTemplate = new TestRestTemplate(HttpClientOption.ENABLE_COOKIES,
+            HttpClientOption.ENABLE_REDIRECTS);
+
+    @Test
+    public void NilkhetJaiRegistrationTestGet() {
+        String response = restTemplate.getForObject(regAppURL, String.class);
+        assertEquals(true, response.contains("<title>Registration</title>"));
+    }
+
+    @Test
+    public void RegistrationTest() {
+        HttpHeaders httpFormHeaders = generateHeader();
+
+        MultiValueMap<String, String> postValueMap = new LinkedMultiValueMap<>();
+        postValueMap.add("email", "munna.cse.ju@gmail.comsok");
+        postValueMap.add("password", "munna");
+        postValueMap.add("confirmPassword", "munna");
+        HttpEntity<MultiValueMap<String, String>> request = new HttpEntity<>(postValueMap, httpFormHeaders);
+        ResponseEntity<String> result = restTemplate.postForEntity(regAppURL, request,
+                String.class);
+        assertThat(result.getStatusCode().equals(HttpStatus.PERMANENT_REDIRECT));
+    }
+
+    private HttpHeaders generateHeader() {
+        HttpHeaders httpFormHeaders = new HttpHeaders();
+        httpFormHeaders.setContentType(MediaType.APPLICATION_FORM_URLENCODED);
+        return httpFormHeaders;
+    }
+
+}
